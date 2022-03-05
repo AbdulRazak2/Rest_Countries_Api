27a0777dfdf846aa0353f5fbc6d7af89cafdf6ab
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-05 13:32:19 +0000

    added wrapper styles

---
+++ src/components/Filters/Filters.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-bottom: 50px;
+  gap: 10px 30px;
+`;
