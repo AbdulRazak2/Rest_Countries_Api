bb5d724bc54c6fde86a7b4e34f2e2f516b3cbd75
EBENEZER ASHONG <ebenezer.ashong@amalitech.org>
2022-03-05 13:12:11 +0000

    add searchPage  with state and use state etc

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -4,3 +4,9 @@ import { Wrapper, CardsList, StyledLoading } from './SearchPage.styles';
 import useCountries from 'Providers/CountriesProvider';
 import { useState } from 'react';
 import { ReactComponent as Icon } from 'assets/loadingIcon.svg';
+
+const SearchPage = () => {
+    const { countries, state } = useCountries();
+    const [selectedValue, setSelectedValue] = useState('');
+    const [inputValue, setInputValue] = useState('');
+};
