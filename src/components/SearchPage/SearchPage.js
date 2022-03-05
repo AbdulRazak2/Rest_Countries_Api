64c2889d5b222c293101262c2e5370c672efbe30
EBENEZER ASHONG <ebenezer.ashong@amalitech.org>
2022-03-05 13:00:38 +0000

    add imprt files

---
+++ src/components/SearchPage/SearchPage.js
@@ -0,0 +1,6 @@
+
+import Filters from 'components/Filters/Filters';
+import { Wrapper, CardsList, StyledLoading } from './SearchPage.styles';
+import useCountries from 'Providers/CountriesProvider';
+import { useState } from 'react';
+import { ReactComponent as Icon } from 'assets/loadingIcon.svg';
