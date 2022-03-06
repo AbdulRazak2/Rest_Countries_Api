5a5c6d80990133d985b37ea091c34484533c239f
blankwriter <prince.eworho@amalitech.org>
2022-03-06 18:59:08 +0000

    provider construct

--- src/Providers/CountriesProvider.js
+++ src/Providers/CountriesProvider.js
@@ -1,1 +1,12 @@
 import React, { useState, useEffect, useContext, useCallback } from 'react';
+export const CountriesProvider = ({ children }) => {
+  const [countries, setCountries] = useState(null);
+  const [state, setState] = useState('loading');
+
+  const findCountryByName = useCallback(
+    (name) => {
+      if (!countries) return;
+      return countries.find((country) => country.name.common === name);
+    },
+    [countries]
+  );
