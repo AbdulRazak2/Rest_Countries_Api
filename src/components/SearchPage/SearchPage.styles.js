119f1ff6c8f992dfad4d63d748d88964e4c0571d
EBENEZER ASHONG <ebenezer.ashong@amalitech.org>
2022-03-07 14:00:29 +0000

    export wrapper

--- src/components/SearchPage/SearchPage.styles.js
+++ src/components/SearchPage/SearchPage.styles.js
@@ -1,1 +1,12 @@
 import styled, { keyframes } from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.veryDarkBlue};
+  padding: 50px 100px;
+  @media (max-width: 1000px) {
+    padding: 50px;
+  }
+  @media (max-width: 500px) {
+    padding: 20px;
+  }
+  `;
